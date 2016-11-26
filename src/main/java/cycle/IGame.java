9396e974ac203ca406c434c8bc6755f69d48cc17
psysrc <psysrc@nottingham.ac.uk>
2016-11-26 21:37:43 +0000

    Updated interface for Game

--- src/main/java/cycle/IGame.java
+++ src/main/java/cycle/IGame.java
@@ -1,5 +1,8 @@
 package cycle;
 
 public interface IGame {
-
+	public void initialise();
+	public void performDay();
+	public void performNight();
+	public void setEntityList(ArrayList<Entity> list);
 }
